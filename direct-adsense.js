c9fd86beedb9bd0b7f3a63f5acedb68c696ec9ea
phanluanvt <phanluanvt@gmail.com>
2026-08-22 16:26:01 -0700

    Make AdSense injection deterministic in generated HTML

--- direct-adsense.js
+++ direct-adsense.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
-const Module = require('module');
 const path = require('path');
 
-const originalJs = Module._extensions['.js'];
-const target = path.join(__dirname, 'server.js');
+const source = path.join(__dirname, 'server.js');
+const patched = path.join('/tmp', 'anngonkhoe-server-adsense.js');
 const adsense = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3937076958073792" crossorigin="anonymous"></script>';
 
-Module._extensions['.js'] = function(mod, filename) {
-  if (path.resolve(filename) === path.resolve(target)) {
-    let code = fs.readFileSync(filename, 'utf8');
-    if (!code.includes('ca-pub-3937076958073792')) {
-      code = code.replace('<style>${css}</style></head>', '<style>${css}</style>' + adsense + '</head>');
-    }
-    return mod._compile(code, filename);
+let code = fs.readFileSync(source, 'utf8');
+if (!code.includes('ca-pub-3937076958073792')) {
+  const marker = '<style>${css}</style></head>';
+  if (!code.includes(marker)) {
+    throw new Error('AdSense inject marker not found in server.js');
   }
-  return originalJs(mod, filename);
-};
+  code = code.replace(marker, '<style>${css}</style>' + adsense + '</head>');
+}
+fs.writeFileSync(patched, code, 'utf8');
+require(patched);
